beb7925cd4fdcdc9cf9ad0b537f943ef38ebc727
Victor Han <liyanghan1112@gmail.com>
2022-07-25 09:36:56 -0500

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Victor Han on 2022/7/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
